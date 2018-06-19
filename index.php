7e6916dd318a5eb33dd48e5f8d3b78302832dc87
FSkay <felipe_menegat@hotmail.com>
2018-06-19 02:53:38 -0300

    BilugaNDO

--- index.php
+++ index.php
@@ -1,17 +1,47 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-    </body>
-</html>
+<?php require_once 'config.php'; ?>
+<?php require_once DBAPI; ?>
+
+<?php include(HEADER_TEMPLATE); ?>
+<?php $db = open_database(); ?>
+
+<h1>Dashboard</h1>
+<hr />
+
+<?php if ($db) : ?>
+
+<div class="row">
+	<div class="col-xs-6 col-sm-3 col-md-2">
+		<a href="customers/add.php" class="btn btn-primary">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-plus fa-5x"></i>
+				</div>
+				<div class="col-xs-12 text-center">
+					<p>Novo Cliente</p>
+				</div>
+			</div>
+		</a>
+	</div>
+
+	<div class="col-xs-6 col-sm-3 col-md-2">
+		<a href="customers" class="btn btn-default">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="col-xs-12 text-center">
+					<p>Clientes</p>
+				</div>
+			</div>
+		</a>
+	</div>
+</div>
+
+<?php else : ?>
+	<div class="alert alert-danger" role="alert">
+		<p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
+	</div>
+
+<?php endif; ?>
+
+<?php include(FOOTER_TEMPLATE); ?>
